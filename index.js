1186a685bcb0712b975718936a9599b7fdd3978a
VGontier <gontiervivien@gmail.com>
2024-02-02 14:56:53 +0100

    fix: handle server error

--- index.js
+++ index.js
@@ -17,33 +17,43 @@ const getPair = async (first, second) => {
 
     processedPairs.add(pairKey);
 
-    const response = await axios.get(`https://neal.fun/api/infinite-craft/pair?first=${first}&second=${second}`, {
-        headers: {
-            'Accept': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
-            'Dnt': '1',
-            'Referer': 'https://neal.fun/infinite-craft/',
-            'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
-            'Sec-Ch-Ua-Mobile': '?0',
-            'Sec-Ch-Ua-Platform': '"Windows"',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0'
-        }
-    });
+    try {
+        const response = await axios.get(`https://neal.fun/api/infinite-craft/pair?first=${first}&second=${second}`, {
+            headers: {
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+                'Dnt': '1',
+                'Referer': 'https://neal.fun/infinite-craft/',
+                'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
+                'Sec-Ch-Ua-Mobile': '?0',
+                'Sec-Ch-Ua-Platform': '"Windows"',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0'
+            }
+            
+        });
 
-    if (!results.has(response.data.result)) {
-        results.add(response.data.result);
-        queue.push(response.data.result);
-        const pairData = {
-            firstParent: first,
-            secondParent: second,
-            result: response.data
-        };
-        console.log(pairData)
-        await appendFile('pairs.json', JSON.stringify(pairData, null, 2) + ',\n');
+        if (!results.has(response.data.result)) {
+            results.add(response.data.result);
+            queue.push(response.data.result);
+            const pairData = {
+                firstParent: first,
+                secondParent: second,
+                result: response.data
+            };
+
+            if(pairData.result.isNew){
+                console.log("New Pair Found: ", pairData.result)
+            }
+            else
+                console.log(pairData)
+            await appendFile('pairs.json', JSON.stringify(pairData, null, 2) + ',\n');
+        }
+    } catch (error) {
+        console.error(`Error getting pair ${first}-${second}: ${error.message}`);
     }
 };
 
